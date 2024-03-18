732712c57d89c1f411fb3922edffa632fbbb677c
Concetta Iuso <conciuso@emeal.nttdata.com>
2024-03-18 17:43:48 +0000

    need to fix the success modal,added pledge and selected card but need to fix that as well

---
+++ JS/success.js
@@ -0,0 +1,18 @@
+// const success = document.getElementById("success-container");
+// const submit = document.getElementsByClassName("pay");
+
+// submit.onclick = function () {
+//     success.style.display = "grid";
+// }
+
+// var closeSuccess = document.getElementById("final-success");
+
+// closeSuccess.onclick = function () {
+//     success.style.display = "none";
+// }
+
+// window.onclick = function (event) {
+//     if (event.target == closeSuccess) {
+//         success.style.display = "none";
+//     }
+// }
